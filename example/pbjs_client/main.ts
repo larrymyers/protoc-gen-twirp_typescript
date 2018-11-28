46442fe4737c43124afaaedc84b8c32fca417e5a
Larry Myers <larry@larrymyers.com>
2018-11-28 10:30:31 -0600

    For easier debugging in the pbjs example log the JSON serialized object to the DOM in addition to the console.

--- example/pbjs_client/main.ts
+++ example/pbjs_client/main.ts
@@ -3,5 +3,8 @@ import {createHaberdasher} from './service.twirp';
 const haberdasher = createHaberdasher('http://localhost:8080');
 
 haberdasher.makeHat({inches: 10})
-    .then((hat) => console.log(hat))
+    .then((hat) => {
+        document.getElementById('debug')!.innerHTML = JSON.stringify(hat, null, "  ");
+        console.log(hat);
+    })
     .catch((err) => console.error(err));
